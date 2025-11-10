3695bbff79698165ff59203a55561ceca67ef3a1
Leeo <mizzking75@gmail.com>
2025-11-11 01:05:21 +0900

    Initial Commit

---
+++ RoutineCamera/RoutineCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoutineCameraApp.swift
+//  RoutineCamera
+//
+//  Created by hyunho lee on 11/11/25.
+//
+
+import SwiftUI
+
+@main
+struct RoutineCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
